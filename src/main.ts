ce61742aa8452be0c813e3df545c363186a4b66a
Ittalian <bothmather@au.com>
2024-07-08 01:04:48 +0900

    ゲーム完成

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { makeBirdEnemy, makeFlameEnemy, makeGuyEnemy, makePlayer, setControls } from "./entities";
 import { k } from "./kaboomCtx";
 import { makeMap } from "./utils";
 
@@ -7,9 +8,9 @@ async function gameSetup() {
         sliceY: 10,
         anims: {
             kirbIdle: 0,
-            kirgInhaling: 1,
+            kirbInhaling: 1,
             kirbFull: 2,
-            kirbInhaleeffect: { from: 3, to: 8, speed: 15, loop: true },
+            kirbInhaleEffect: { from: 3, to: 8, speed: 15, loop: true },
             shootingStar: 9,
             flame: { from: 36, to: 37, speed: 4, loop: true },
             guyIdle: 18,
@@ -33,7 +34,41 @@ async function gameSetup() {
             k.fixed(),
         ]);
 
-        k.add(level1Layout)
+        k.add(level1Layout);
+
+        const kirb = makePlayer(
+            k,
+            level1SpawnPoints.player[0].x,
+            level1SpawnPoints.player[0].y
+        );
+
+        setControls(k, kirb);
+        k.add(kirb);
+        k.camScale(k.vec2(0.7));
+        k.onUpdate(() => {
+            if (kirb.pos.x < level1Layout.pos.x + 432)
+                k.camPos(kirb.pos.x + 500, 870);
+        });
+
+        for (const flame of level1SpawnPoints.flame) {
+            makeFlameEnemy(k, flame.x, flame.y);
+        }
+
+        for (const guy of level1SpawnPoints.guy) {
+            makeGuyEnemy(k, guy.x, guy.y);
+        }
+
+        for (const bird of level1SpawnPoints.bird) {
+            const possibleSpeeds = [100, 200, 300];
+            k.loop(10, () => {
+                makeBirdEnemy(
+                    k,
+                    bird.x,
+                    bird.y,
+                    possibleSpeeds[Math.floor(Math.random() * possibleSpeeds.length)]
+                );
+            });
+        }
     });
 
     k.go("level-1");
